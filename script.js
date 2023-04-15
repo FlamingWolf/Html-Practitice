55dffff9bb15b849ae290cd9ff4dbba0056de0fd
FlamingWolf <46968587+FlamingWolf@users.noreply.github.com>
2023-04-14 23:52:16 -0500

    Finshed the special CODE Buttons

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function mulit(p1, p2) {
-    document.getElementById("demo").innerHTML = p1*p2;
-    return p1 * p2;
-    
+    document.getElementById("demo").innerHTML = p1*p2 + " SPECIAL CODE PLAYED";
+    return p1 * p2;  
 }
